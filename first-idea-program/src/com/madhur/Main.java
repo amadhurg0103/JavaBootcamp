4a2a80aedf6d80ff30884de0863130d0e4a477cb
amadhurg0103 <a.madhurg.n0103@gmail.com>
2024-10-18 19:49:22 +0530

    edit

--- first-idea-program/src/com/madhur/Main.java
+++ first-idea-program/src/com/madhur/Main.java
@@ -1,7 +1,14 @@
 package com.madhur;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
          System.out.println("Hello world!");
+         Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        System.out.println(input.next());
+        System.out.println(input.nextLine());
     }
 }
