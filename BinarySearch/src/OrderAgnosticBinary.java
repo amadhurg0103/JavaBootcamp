080a657bb07181913caf15893d2d62d6171ee153
amadhurg0103 <a.madhurg.n0103@gmail.com>
2024-10-30 12:40:09 +0530

    orderagnosticbs

--- BinarySearch/src/OrderAgnosticBinary.java
+++ BinarySearch/src/OrderAgnosticBinary.java
@@ -1,5 +1,36 @@
 public class OrderAgnosticBinary {
     public static void main(String[] args) {
+           int [] arr={101,100,95,78,56,45,12,2,-8};
+           int target=45;
+           int ans=orderagnosticBS(arr,target);
+        System.out.println(ans);
+    }
 
+    static int orderagnosticBS(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if (arr[0]<arr[arr.length-1]) {
+                if (arr[mid] < target) {
+                    start = mid + 1;
+                } else if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+             if (arr[0]>arr[arr.length-1])  {
+                 if (arr[mid]<target){
+                     end=mid-1;
+                 }else if (arr[mid]>target){
+                     start=mid+1;
+                 }else {
+                     return mid;
+                 }
+             }
+
+        }
+        return -1;
     }
 }
